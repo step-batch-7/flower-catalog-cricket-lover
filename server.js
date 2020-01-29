00a609453035e141e7a8a4a679b9abf50efc111c
phaneendra <phaneendraandukuri@gmail.com>
2020-01-29 10:00:05 +0530

    changed net module to http module

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { Server } = require('net');
+const { Server } = require('http');
 
 const CONTENT_TYPES = {
   txt: 'text/plain',
@@ -12,21 +12,16 @@ const CONTENT_TYPES = {
   pdf: 'application/pdf'
 };
 
-const serveBadRequestPage = function() {
-  const notFoundPage = `
+const serveBadRequestPage = function(req, res) {
+  const badRequestPage = `
 <html>
   <body>
     <h2>404:File not found</h2>
   </body>
 </html>`;
-  const defaultResponse = [
-    'HTTP/1.1 404 Not Found',
-    'Content-type: text/html',
-    `Content-length:${notFoundPage.length}`,
-    '',
-    notFoundPage
-  ];
-  return defaultResponse.join('\n');
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(badRequestPage);
 };
 
 const getExistingComments = function() {
@@ -35,10 +30,14 @@ const getExistingComments = function() {
   return [];
 };
 
-const createPage = function(filename) {
+const STATIC_FOLDER = `${__dirname}/public`;
+
+const createPage = function(req, res) {
   const existingComments = getExistingComments();
-  let fileContent = fs.readFileSync(filename);
-  const [, , extension] = filename.split('.');
+  let filename = `${req.url}`;
+  if (filename === '/') filename = `/index.html`;
+  let fileContent = fs.readFileSync(`${STATIC_FOLDER}${filename}`);
+  const [, extension] = filename.split('.');
 
   const updateGuestBook = function(existingComments, newComment) {
     const { name, comment, date } = newComment;
@@ -52,39 +51,16 @@ const createPage = function(filename) {
     fileContent = fileContent.toString();
     updatedFileContent = fileContent.replace('__comments__', name);
   }
-
-  const response = [
-    'HTTP/1.1 200 OK',
-    `Content-type: ${CONTENT_TYPES[extension]}`,
-    `Content-length: ${updatedFileContent.length}`,
-    'Connection:closed',
-    '',
-    ''
-  ];
-
-  return [response.join('\n'), updatedFileContent];
+  const contentType = CONTENT_TYPES[extension];
+  res.setHeader('Content-Type', contentType);
+  res.end(updatedFileContent);
 };
 
-const showUserPage = function() {
-  const defaultResponse = [
-    'HTTP/1.1 301',
-    `Content-length: 0`,
-    'Location: http://localhost:9000/guestBook.html',
-    '',
-    ''
-  ];
-
-  return [defaultResponse.join('\n')];
-};
-
-const generateResponse = function(text) {
-  const comments = getExistingComments();
-  const [request, ...headerAndContent] = text.split('\n');
-  let [method, filename] = request.split(' ');
-  if (filename === '/') filename = '/index.html';
-
-  if (method === 'POST' && filename === '/showUserPage') {
-    const userDetails = headerAndContent[headerAndContent.length - 1];
+const showUserPage = function(req, res) {
+  const existingComments = getExistingComments();
+  let userDetails = '';
+  req.on('data', chunk => (userDetails += chunk));
+  req.on('end', () => {
     const keyValuePairs = userDetails.split('&');
     const pairs = keyValuePairs.reduce((context, pair) => {
       const [key, value] = pair.split('=');
@@ -92,36 +68,33 @@ const generateResponse = function(text) {
       return context;
     }, {});
     pairs['date'] = new Date();
-    comments.push(pairs);
-    fs.writeFileSync('data/comments.json', JSON.stringify(comments), 'utf8');
-    return showUserPage();
-  }
-
-  if (!fs.existsSync(`./public${filename}`)) {
-    return [serveBadRequestPage()];
-  }
-  return createPage(`./public${filename}`);
+    existingComments.push(pairs);
+    fs.writeFileSync(
+      'data/comments.json',
+      JSON.stringify(existingComments),
+      'utf8'
+    );
+  });
+  res.statusCode = 301;
+  res.setHeader('Location', 'http://localhost:9000/guestBook.html');
+  res.end();
 };
 
-const handleRequest = function(socket) {
-  const remote = `${socket.remoteAddress}:${socket.remotePort}`;
-  console.warn('new connection', remote);
-  socket.setEncoding('utf8');
-  socket.on('data', text => {
-    console.warn(text);
-    const contents = generateResponse(text);
-    contents.forEach(res => socket.write(res));
-  });
-  socket.on('close', hadError => console.warn('closed', hadError));
-  socket.on('end', () => console.warn(remote, 'ended'));
+const handleRequest = function(req, res) {
+  const filename = req.url;
+  const method = req.method;
+  if (method === 'POST' && filename === '/showUserPage') {
+    return showUserPage(req, res);
+  }
+  if (fs.existsSync(`./public${filename}`)) {
+    return createPage(req, res);
+  }
+  return serveBadRequestPage(req, res);
 };
 
 const main = function() {
-  const server = new Server();
-  server.on('error', err => console.warn('error', err));
-  server.on('connection', handleRequest);
+  const server = new Server(handleRequest);
   server.on('listening', () => console.warn('server is listening'));
-  server.on('close', () => console.warn('server closed'));
   server.listen(9000);
 };
 main();
