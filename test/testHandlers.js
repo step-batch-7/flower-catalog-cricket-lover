4e7801d500e0b3f93b6f9d192d48e057b9854667
phaneendra <phaneendraandukuri@gmail.com>
2020-01-31 13:57:11 +0530

    added tests

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -12,9 +12,13 @@ describe('GET index.html', function() {
   });
   it('should get the requested file when correct path is given', function(done) {
     request(app.serve.bind(app))
-      .get('/ageratum.html')
-      .expect('Content-Type', 'text/html')
-      .expect(200, done)
-      .expect(/ageratum/);
+      .get('/js/hideJar.js')
+      .expect('Content-Type', 'application/javascript')
+      .expect(200, done);
+  });
+  it('should give not found when incorrect path is given', function(done) {
+    request(app.serve.bind(app))
+      .get('/badFile')
+      .expect(404, done);
   });
 });
