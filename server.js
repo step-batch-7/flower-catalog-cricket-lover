2070449ab8b540e68e9041f8021fcdcb780d971c
phaneendra <phaneendraandukuri@gmail.com>
2020-02-25 15:18:44 +0530

    updated package.json

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const {Server} = require('http');
-const {app} = require('./handler');
+const { Server } = require('http');
+const { app } = require('./handler');
+const port = process.env.PORT || 9000;
 
 const main = function() {
   const server = new Server(app.serve.bind(app));
-  server.on('listening', () => console.warn('server is listening'));
-  server.listen(9000);
+  server.on('listening', () => process.stdout.write('server is listening'));
+  server.listen(port);
 };
 main();
